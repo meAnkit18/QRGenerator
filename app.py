f307695bfc9bfc8d0d45d5f24281b2b01e20c956
Ankit Kumar <ankit1842kumar@gmail.com>
2025-07-24 21:12:04 +0530

    learing project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask , render_template, request, send_file
+import qrcode
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method=='POST':
+        text = request.form['text']
+        qrcode.make(text).save('.png')
+        return send_file('.png', mimetype='image/png')
+    return render_template('index.html')
+
+if __name__== "__main__":
+    app.run(debug=True)
